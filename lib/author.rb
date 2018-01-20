2651aafce1fe86fb6785ec957355ed6150436f89
Wayne Buxton <waynebuxton2184@yahoo.com>
2018-01-20 00:08:40 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,35 +1,35 @@
 require "pry"
 class Author
 
-attr_accessor :name
+  attr_accessor :name
 
-@@post_count = 0
+  @@post_count = 0
 
-def initialize(name)
-  @name = name
-  @posts = []
-end
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
 
-def posts
-  @posts
-end
+  def posts
+    @posts
+  end
 
-def add_post(post)
-  @posts << post
-  @@post_count += 1
-  post.author = self
-end
+  def add_post(post)
+    @posts << post
+    @@post_count += 1
+    post.author = self
+  end
 
-def add_post_by_title(title)
-  post = Post.new(title)
-  @posts << post
-  @@post_count += 1
-  post.author = self
-  post.name = name
-  #binding.pry
-end
+  def add_post_by_title(title)
+    post = Post.new(title)
+    @posts << post
+    @@post_count += 1
+    post.author = self
+    post.name = name
+    #binding.pry
+  end
 
-def self.post_count
-  @@post_count
-end
+  def self.post_count
+    @@post_count
+  end
 end
